3211b0660cf6b2885a4348dcd9d93acb92199645
arantzapl <arantza.perezlaborda@gmail.com>
2024-02-23 12:46:34 +0100

    Cambio por error, queda como estaba

--- MF0966/UF2214/src/main/java/com/ipartek/formacion/uf2214/accesodatos/PostAccesoDatos.java
+++ MF0966/UF2214/src/main/java/com/ipartek/formacion/uf2214/accesodatos/PostAccesoDatos.java
@@ -1,6 +1,7 @@
 package com.ipartek.formacion.uf2214.accesodatos;
 
 import static com.ipartek.formacion.uf2214.accesodatos.AccesoDatosJpa.enTransaccion;
+
 import java.util.List;
 
 import com.ipartek.formacion.uf2214.entidades.Post;
@@ -14,13 +15,13 @@ public class PostAccesoDatos {
 	}
 	
 	public static List<Post> obtenerTodos() {
-		return enTransaccion(em -> em.createQuery("select p from Post p order by p.fecha desc", Post.class).getResultList());
+		return enTransaccion(em -> em.createQuery("select p from Post p join fetch p.usuario join fetch p.usuario.rol order by p.fecha desc", Post.class).getResultList());
 	}
 	
 	public static List<Post> obtenerTodos(int offset, int tamanoPagina) {
 		return enTransaccion(em -> {
 			var consulta = em.createQuery("select p from Post p order by p.fecha desc", Post.class);
-			
+
 			consulta.setFirstResult(offset);
 			consulta.setMaxResults(tamanoPagina);
 			
@@ -29,6 +30,6 @@ public class PostAccesoDatos {
 	}
 	
 	public static List<Post> obtenerPorUsuario(long idUsuario) {
-		return enTransaccion(em -> em.createQuery("select p from Post p order by p.fecha desc where p.usuario.id = :idUsuario", Post.class).setParameter("idUsuario", idUsuario).getResultList());
+		return enTransaccion(em -> em.createQuery("select p from Post p join fetch p.usuario join fetch p.usuario.rol where p.usuario.id = :idUsuario order by p.fecha desc", Post.class).setParameter("idUsuario", idUsuario).getResultList());
 	}
 }
--- MF0966/UF2214/src/main/java/com/ipartek/formacion/uf2214/accesodatos/UsuarioAccesoDatos.java
+++ MF0966/UF2214/src/main/java/com/ipartek/formacion/uf2214/accesodatos/UsuarioAccesoDatos.java
@@ -4,8 +4,9 @@ import static com.ipartek.formacion.uf2214.accesodatos.AccesoDatosJpa.enTransacc
 
 import java.util.List;
 
-import com.ipartek.formacion.uf2214.entidades.Post;
 import com.ipartek.formacion.uf2214.entidades.Usuario;
+
+import jakarta.persistence.Query;
 
 public class UsuarioAccesoDatos {
 	public static void insertar(Usuario usuario) {
@@ -14,59 +15,62 @@ public class UsuarioAccesoDatos {
 			return null;
 		});
 	}
-	
+
 	public static Usuario buscarPorNickName(String nickName) {
 		return enTransaccion(em -> {
-			var usuarios = em.createQuery("select u from Usuario u where u.nickName = :nickName", Usuario.class).setParameter("nickName", nickName).getResultList();
-			
-			if(usuarios.size() == 1) {
+			var usuarios = em
+					.createQuery("select u from Usuario u join fetch u.rol where u.nickName = :nickName", Usuario.class)
+					.setParameter("nickName", nickName).getResultList();
+
+			if (usuarios.size() == 1) {
 				return usuarios.get(0);
 			} else {
 				return null;
 			}
 		});
 	}
-	
+
 	public static List<Usuario> obtenerSeguidores(long id) {
-		return enTransaccion(em -> em.createQuery("select u from Usuario u where :id in u.seguidorDe.id", Usuario.class).setParameter("id", id).getResultList());
+		return enTransaccion(em -> em.createQuery(
+				"select seguidores from Usuario seguidores join seguidores.seguidorDe seguido where seguido.id = :id",
+				Usuario.class).setParameter("id", id).getResultList());
 	}
-	
+
 	public static void agregarSeguidor(long id, long seguirAId) {
 		enTransaccion(em -> {
-			Usuario usuario = em.find(Usuario.class, id);
-			Usuario seguirA = em.find(Usuario.class, seguirAId);
-			
-			usuario.getSeguidorDe().remove(seguirA);
-			
-			em.merge(usuario);
-			
+			Query query = em.createNativeQuery(
+					"INSERT INTO usuarios_seguidores (usuario_id, seguidor_de_id) VALUES (:id, :seguidorDeId)");
+
+			query.setParameter("id", id);
+			query.setParameter("seguidorDeId", seguirAId);
+			query.executeUpdate();
+
 			return null;
 		});
 	}
-	
-	public static void agregarBloqueado(long id, long bloqueadosId) {
+
+	public static void quitarSeguidor(long id, long seguirAId) {
 		enTransaccion(em -> {
-			Usuario usuario = em.find(Usuario.class, id);
-			Usuario bloqueados = em.find(Usuario.class, bloqueadosId);
-			
-			usuario.getBloqueados().remove(bloqueados);
-			
-			em.merge(usuario);
-			
+			Query query = em.createNativeQuery(
+					"DELETE FROM usuarios_seguidores WHERE usuario_id = :id AND seguidor_de_id = :seguidorDeId");
+
+			query.setParameter("id", id);
+			query.setParameter("seguidorDeId", seguirAId);
+			query.executeUpdate();
+
 			return null;
 		});
 	}
-	
-//	public static void agregarDenunciado(long id, Denuncia denuncia) {
-//		enTransaccion(em -> {
-//			Usuario usuario = em.find(Usuario.class, id);
-//			Usuario denunciado = em.find(Usuario.class, denunciadoId);
-//			
-//			usuario.getDenunciasRealizadas().remove(denunciado);
-//			
-//			em.merge(usuario);
-//			
-//			return null;
-//		});
-//	}
+
+	public static void agregarBloqueado(long id, long bloqueadoId) {
+		enTransaccion(em -> {
+			Query query = em.createNativeQuery(
+					"INSERT INTO usuarios_bloqueados (usuario_id, bloqueado_id) VALUES (:id, :bloqueadoId)");
+			query.setParameter("id", id);
+			query.setParameter("bloqueadoId", bloqueadoId);
+			query.executeUpdate();
+
+			return null;
+		});
+	}
 }
