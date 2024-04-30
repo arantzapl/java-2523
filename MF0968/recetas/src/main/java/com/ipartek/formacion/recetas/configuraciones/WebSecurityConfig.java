262cd80f9ae6c91fb1ffb4e391a8fb414c288ba0
arantzapl <arantza.perezlaborda@gmail.com>
2024-04-30 10:08:27 +0200

    Spring Security implementado a Recetas 30.04.2024

---
+++ MF0968/recetas/src/main/java/com/ipartek/formacion/recetas/configuraciones/WebSecurityConfig.java
@@ -0,0 +1,47 @@
+package com.ipartek.formacion.recetas.configuraciones;
+import javax.sql.*;
+
+import org.springframework.beans.factory.annotation.*;
+import org.springframework.context.annotation.*;
+import org.springframework.security.config.annotation.authentication.builders.*;
+import org.springframework.security.config.annotation.web.builders.*;
+import org.springframework.security.config.annotation.web.configuration.*;
+import org.springframework.security.crypto.bcrypt.*;
+import org.springframework.security.crypto.password.*;
+import org.springframework.security.web.*;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig {
+	// https://www.baeldung.com/spring-security-jdbc-authentication
+	// AUTENTICACIÓN
+	@Autowired
+	private DataSource dataSource;
+	@Autowired
+	public void configureGlobal(AuthenticationManagerBuilder auth)
+	  throws Exception {
+	    auth.jdbcAuthentication().dataSource(dataSource);
+	}
+
+	@Bean
+	PasswordEncoder passwordEncoder() {
+	    return new BCryptPasswordEncoder();
+	}
+
+	// AUTORIZACIÓN
+	@Bean
+	SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+		http.authorizeHttpRequests(
+				(requests) -> requests
+					.requestMatchers("/ingrediente").hasRole("ADMIN")
+					.anyRequest().authenticated()
+				)
+				.formLogin((form) -> form
+//						.loginPage("/login")
+						.permitAll()
+				)
+//				.logout((logout) -> logout.permitAll())
+				;
+		return http.build();
+	}
+}
--- MF0968/recetas/src/main/java/com/ipartek/formacion/recetas/presentacion/controladores/IndexController.java
+++ MF0968/recetas/src/main/java/com/ipartek/formacion/recetas/presentacion/controladores/IndexController.java
@@ -21,26 +21,26 @@ import jakarta.validation.Valid;
 public class IndexController {
 	@Autowired
 	private RecetaService servicio;
-
+	
 	@GetMapping("ingredientes")
 	public String listadoIngredientes(Model modelo) {
 		modelo.addAttribute("ingredientes", servicio.listarIngredientes());
 		return "ingredientes";
 	}
-
+	
 	@GetMapping("ingrediente")
 	public String formularioIngrediente(Ingrediente ingrediente) {
 		return "ingrediente";
 	}
-
+	
 	@PostMapping("ingrediente")
 	public String postIngrediente(@Valid Ingrediente ingrediente, BindingResult bindingResult) {
 		if(bindingResult.hasErrors()) {
 			return "ingrediente";
 		}
-
+		
 		servicio.anadirIngrediente(ingrediente);
-
+		
 		return "redirect:/ingredientes";
 	}
 
@@ -48,7 +48,7 @@ public class IndexController {
 	public String formularioPlato(Plato plato, Model modelo) {
 		modelo.addAttribute("niveles", servicio.listarDificultades());
 		modelo.addAttribute("tipos", servicio.listarTiposCocina());
-
+		
 		return "plato";
 	}
 
@@ -57,19 +57,19 @@ public class IndexController {
 		if(bindingResult.hasErrors()) {
 			return "plato";
 		}
-
+		
 		servicio.anadirPlato(plato);
-
+		
 		return "redirect:/platos";
 	}
 
-	@GetMapping("platos")
+	@GetMapping({"/", "platos"})
 	public String listadoPlatos(Model modelo) {
 		modelo.addAttribute("platos", servicio.listadoPlatos());
-
+		
 		return "platos";
 	}
-
+	
 	@GetMapping("plato/{id}")
 	public String formularioIngredientes(@PathVariable Long id, PlatoIngrediente platoIngrediente, Model modelo) {
 		modelo.addAttribute("plato", servicio.verPlato(id));
@@ -78,32 +78,32 @@ public class IndexController {
 
 		return "plato-ingredientes";
 	}
-
+	
 	@PostMapping("plato/{idPlato}")
 	public String postIngredientes(@PathVariable Long idPlato, @Valid PlatoIngrediente platoIngrediente, BindingResult bindingResult, Model modelo) {
 		// TODO revisar el proceso
-
+		
 		System.out.println(platoIngrediente);
-
+		
 		var plato = Plato.builder().id(idPlato).build();
 		platoIngrediente.setPlato(plato);
-
+		
 		System.out.println(platoIngrediente);
-
+		
 		if(bindingResult.hasErrors()) {
 			System.out.println(bindingResult);
-
+			
 			modelo.addAttribute("plato", servicio.verPlato(idPlato));
 			modelo.addAttribute("platoIngredientes", servicio.verIngredientesPlato(idPlato));
 			modelo.addAttribute("ingredientes", servicio.listarIngredientes());
-
+			
 			return "plato-ingredientes";
 		}
-
+		
 		servicio.anadirIngredienteAPlato(platoIngrediente);
-
+		
 		System.out.println(platoIngrediente);
-
+		
 		return "redirect:/plato/" + idPlato;
 	}
 
