9c394085e6cfaf83d3bd80808b3152b02a465856
arantzapl <arantza.perezlaborda@gmail.com>
2024-05-02 11:39:41 +0200

    Frontend de recetas y añadir borrado y modificado de platos 02.05.2024

--- MF0968/recetas/src/main/java/com/ipartek/formacion/recetas/servicios/RecetaService.java
+++ MF0968/recetas/src/main/java/com/ipartek/formacion/recetas/servicios/RecetaService.java
@@ -12,35 +12,47 @@ public interface RecetaService {
 	default void anadirIngrediente(Ingrediente ingrediente) {
 		throw new UnsupportedOperationException("NO IMPLEMENTADO");
 	}
-
-
+	
 	default Iterable<Ingrediente> listarIngredientes() {
 		throw new UnsupportedOperationException("NO IMPLEMENTADO");
 	}
-
+	
 	default Iterable<Dificultad> listarDificultades() {
 		throw new UnsupportedOperationException("NO IMPLEMENTADO");
 	}
-
+	
 	default Iterable<TipoCocina> listarTiposCocina() {
 		throw new UnsupportedOperationException("NO IMPLEMENTADO");
 	}
-
+	
 	default void anadirPlato(Plato plato) {
 		throw new UnsupportedOperationException("NO IMPLEMENTADO");
 	}
+
+	default void modificarPlato(Plato plato) {
+		throw new UnsupportedOperationException("NO IMPLEMENTADO");
+	}
+
+	default void borrarPlato(Long id) {
+		throw new UnsupportedOperationException("NO IMPLEMENTADO");
+	}
+
 	default void anadirIngredienteAPlato(PlatoIngrediente platoIngrediente) {
 		throw new UnsupportedOperationException("NO IMPLEMENTADO");
 	}
+
 	default void registrarUsuario(Usuario usuario) {
 		throw new UnsupportedOperationException("NO IMPLEMENTADO");
 	}
+
 	default void loginUsuario(Usuario usuario) {
 		throw new UnsupportedOperationException("NO IMPLEMENTADO");
 	}
+
 	default Iterable<Plato> listadoPlatos() {
 		throw new UnsupportedOperationException("NO IMPLEMENTADO");
 	}
+
 	default Plato verPlato(Long id) {
 		throw new UnsupportedOperationException("NO IMPLEMENTADO");
 	}
@@ -48,7 +60,7 @@ public interface RecetaService {
 	default Iterable<PlatoIngrediente> verIngredientesPlato(Long id) {
 		throw new UnsupportedOperationException("NO IMPLEMENTADO");
 	}
-
+	
 	default void valorarPlato(Valoracion valoracion) {
 		throw new UnsupportedOperationException("NO IMPLEMENTADO");
 	}
